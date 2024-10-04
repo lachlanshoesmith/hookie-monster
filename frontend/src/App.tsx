78283e0f13e8ce69e8a7455a3cd5fed4f7c3b43c
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2024-10-04 16:25:09 +1000

    clarify how api call works

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -40,7 +40,7 @@ function App() {
       features: [
         'The useState hook to track the amount of cookies Hookie Monster has eaten.',
         "A ternary operator based on the state to determine whether Hookie Monster says 'cookie' or 'cookies' based on the amount of cookies he's eaten.",
-        'Hookie Monster shares his secret cookie recipe when you feed him 100 cookies. The recipe is grabbed from an API endpoint inside a useEffect hook.',
+        'Hookie Monster shares his secret cookie recipe when you feed him 100 cookies. The recipe is grabbed from an API endpoint with `fetch` inside a useEffect hook.',
         'A button at the top of the screen lets the user switch between Count von CounterExample and Hookie Monster modes.',
       ],
       todos: [
@@ -51,7 +51,7 @@ function App() {
       features: [
         'The useState hook to track the amount of cookies Hookie Monster has eaten.',
         "A ternary operator based on the state to determine whether Hookie Monster says 'cookie' or 'cookies' based on the amount of cookies he's eaten.",
-        'Hookie Monster shares his secret cookie recipe when you feed him 100 cookies. The recipe is grabbed from an API endpoint inside a useEffect hook.',
+        'Hookie Monster shares his secret cookie recipe when you feed him 100 cookies. The recipe is grabbed from an API endpoint with `fetch` inside a useEffect hook.',
         'A button at the top of the screen lets the user switch between Count von CounterExample and Hookie Monster modes.',
         'The useContext hook to switch between the Count von CounterExample and Hookie Monster modes.',
       ],
